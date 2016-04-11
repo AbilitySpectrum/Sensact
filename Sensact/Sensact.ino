4825a56de880fd588380a6d4e1eb0532b6793df8
Yih Lerh Huang <ylh@gmx.com>
2016-04-11 14:05:13 -0400

    modes for Sensact v2
    - local p5 files
    - #ifdefs for the two versions
    - remove gyro capabilities for now

--- Sensact/Sensact.ino
+++ Sensact/Sensact.ino
@@ -1,7 +1,14 @@
 /* Sensact_v3 : 20151106 : 20151026 : ylh
-
-This version works with v1 of Sensact board using the Pro Micro
-
+ 
+ 20160407 : now being modified for Sensact board v2, check defines
+ 
+ version in ~/0/1/Sensact
+ - p5 code in configSensact
+ - Arduino code in Sensact
+ 
+ ==
+ 20151026 version works with v1 of Sensact board using the Pro Micro
+ 
  modified from test_Sensact_v2 to interact with 
  p5 version of configSensact (~/0/p5/configSensact)
  
@@ -10,7 +17,7 @@ This version works with v1 of Sensact board using the Pro Micro
  a) sends config info to p5, one time begin with "9999,...."
  b) receive config info from p5, any number of times
  "0,...."
- c) send input readings to p5, every x msec
+ c) return input readings to p5, every x msec
  
  "9" - Senseact goes into Run mode
  a) no output to p5
@@ -30,6 +37,57 @@ const int USE_BT = 1;
 
 #include <Keyboard.h>
 #include <Wire.h>
+
+// for MPU6050/GY521 ====================
+// OpMode = 0 -> teapot
+//            = 1 click function by movement distance
+//            = 2 mouse function by accel
+//            = 3 write out YPR values
+int OpMode = 2;  //must not be in debug if OpMode==1
+
+#include "pitches.h"
+#include "AACgyromouse.h"
+#include "AACgyroclick.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+MPU6050 mpu;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { 
+   '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+// standalone 1
+GyroClick gClick;
+
+// standalone 2
+VectorInt16 gyro;
+GyroMouse gMouse;
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+   mpuInterrupt = true;
+}
+
+//========== MPU6050 ==============
+
 #include <L3G.h>
 L3G my_gyro;
 
@@ -84,10 +142,17 @@ void setup() {
    }
    // set the digital pin as output:
    //pinMode(ledPin, OUTPUT);
-   pinMode(SENSACT_IN1, INPUT);
+
    pinMode(SENSACT_IN2, INPUT);
    pinMode(SENSACT_IN3, INPUT);
+#ifdef SENSACT_V1
+   pinMode(SENSACT_IN1, INPUT);
    pinMode(SENSACT_IN4, INPUT);
+#endif
+#ifdef SENSACT_V2
+   pinMode(SENSACT_IN1A, INPUT);
+   pinMode(SENSACT_IN1B, INPUT);
+#endif
 
    pinMode(SENSACT_RED, OUTPUT);
    pinMode(SENSACT_GREEN, OUTPUT);
@@ -99,13 +164,18 @@ void setup() {
 
    pinMode(SENSACT_OUT1, OUTPUT);
    pinMode(SENSACT_OUT2, OUTPUT);
+#ifdef SENSACT_V1
    pinMode(SENSACT_OUT3, OUTPUT);
+#endif
 
    digitalWrite( ledPin, HIGH);
 
    for( int i=0; i<nInputs; i++ ) lastRead[i] = 0;
    for( int j=0; j<nOutputs; j++) {
-      digitalWrite( control[j], LOW );
+      if( control[j] == SENSACT_BUZZER) 
+         noTone(SENSACT_BUZZER);
+      else
+         digitalWrite( control[j], LOW );
       whenOn[j] = 0;
       onOff[j] = 0;
    }
@@ -118,6 +188,7 @@ void setup() {
    //Serial.println("2");
    gyro_setup();
    //Serial.println("3");
+   mpu_setup();
 
    load_config();
 
@@ -135,7 +206,10 @@ void loop()
 
    for( int j=0; j<nOutputs; j++ ) {
       if( (currentMillis - whenOn[j]) > beep_interval ) {
-         digitalWrite( control[j], LOW );
+         if( control[j] == SENSACT_BUZZER )
+            ;  //noTone(SENSACT_BUZZER);
+         else     
+            digitalWrite( control[j], LOW );
          onOff[j] = 0;
       }
    }
@@ -364,13 +438,25 @@ void report_signals() {
 
    //if( whichSerial == 0 ) { //ylh kludge
    if( USE_GYRO == 0 ) {
-      Serial.print(gyro_read());
+
+#ifdef SENSACT_V1
+      Serial.print( SENSACT_IN1 ); //Serial.print(gyro_read());
       Serial.print(",");
       Serial.print(map(analogRead( SENSACT_IN2 ), 0, 1023, 0, 100));
       Serial.print(",");      
       Serial.print(map(analogRead( SENSACT_IN3 ), 0, 1023, 0, 100 ));
       Serial.print(",");
       Serial.print(map(analogRead( SENSACT_IN4 ), 0, 1023, 0, 100 ));
+#endif
+#ifdef SENSACT_V2
+      Serial.print(map(analogRead( SENSACT_IN1A ), 0, 1023, 0, 100));
+      Serial.print(",");
+      Serial.print(map(analogRead( SENSACT_IN1B ), 0, 1023, 0, 100));
+      Serial.print(",");
+      Serial.print(map(analogRead( SENSACT_IN2 ), 0, 1023, 0, 100));
+      Serial.print(",");      
+      Serial.print(map(analogRead( SENSACT_IN3 ), 0, 1023, 0, 100 ));
+#endif
       Serial.print("\n");
 
    } 
@@ -408,12 +494,19 @@ void process_signals() {
 
       if( USE_GYRO == 0 ) {
          switch(i) {
+#ifdef SENSACT_V1
          case 0: 
             val = (byte) gyro_read();
             break;
          default:
             val = (byte) map(analogRead( map_in[i-1] ), 0, 1023, 0, 100);
             break;
+#endif
+#ifdef SENSACT_V2
+         default:
+            val = (byte) map(analogRead( map_in[i] ), 0, 1023, 0, 100);
+            break;
+#endif
          }
       } 
       else {
@@ -454,11 +547,17 @@ void process_signals() {
                   onOff[j] ++;
                }
                break;
+            case BUZZER:
+               if( config[offset+j] ) {
+                  tone(control[j], NOTE_C7, 250); //digitalWrite(control[j], HIGH);
+                  ledTime = whenOn[j] = currentMillis;
+                  onOff[j] ++;
+               }
+               break;
                // the following cases have the same behaviour
             case RELAY_A:   
             case RELAY_B:  
             case LED: 
-            case BUZZER:
                if( config[offset+j] ) {
                   digitalWrite(control[j], HIGH);
                   ledTime = whenOn[j] = currentMillis;
@@ -509,15 +608,306 @@ byte gyro_read () {
    0, 100) );
 }
 
-void beep(int j) {
-   if (j<1) return;
-      for( int i=0; i<j; i++ ) {
-      digitalWrite( SENSACT_BUZZER, HIGH );
-      delay(10);
-      digitalWrite( SENSACT_BUZZER, LOW );
-      delay(150);
+void mpu_setup() {
+   // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+   Wire.begin();
+   TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+   Fastwire::setup(400, true);
+#endif
+
+   // initialize serial communication
+   // (115200 chosen because it is required for Teapot Demo output, but it's
+   // really up to you depending on your project)
+   Serial.begin(57600); // 38400
+
+      if( OpMode==0 || OpMode==3 ) while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+   // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
+   // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
+   // the baud timing being too misaligned with processor ticks. You must use
+   // 38400 or slower in these cases, or use some kind of external separate
+   // crystal solution for the UART timer.
+
+   // initialize device
+   Serial.println(F("Initializing I2C devices..."));
+   mpu.initialize();
+
+   // verify connection
+   Serial.println(F("Testing device connections..."));
+   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+
+   // wait for ready
+   if (OpMode==0 && OpMode==3) { //ylh: only when feeding data to Processing
+
+      Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+      while (Serial.available() && Serial.read()); // empty buffer
+      while (!Serial.available());                 // wait for data
+      while (Serial.available() && Serial.read()); // empty buffer again
+   } 
+
+
+   // load and configure the DMP
+   Serial.println(F("Initializing DMP..."));
+   devStatus = mpu.dmpInitialize();
+
+   // supply your own gyro offsets here, scaled for min sensitivity
+   mpu.setXAccelOffset(0);
+   mpu.setYAccelOffset(0);
+   mpu.setZAccelOffset(0); // 1688 factory default for my test chip
+
+   mpu.setXAccelOffset(-2484);
+   mpu.setYAccelOffset(430);
+   mpu.setZAccelOffset(1193);
+   mpu.setXGyroOffset(27);
+   mpu.setYGyroOffset(-44);
+   mpu.setZGyroOffset(59);
+
+
+   // make sure it worked (returns 0 if so)
+   if (devStatus == 0) {
+      // turn on the DMP, now that it's ready
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
+
+      // enable Arduino interrupt detection
+      Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+      attachInterrupt(0, dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
+
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+   } 
+   else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+   }
+   if( OpMode ==1 || OpMode==2 ) {
+      gMouse.mouseReset();
    }
 }
+
+void mpu_sensitivity() { 
+   gMouse.setSensitivity( (int) 50 ); /// ylh ******** fold into setprofile
+   gClick.setSensitivity( (int) 50 );
+}    
+
+void mpu_loop() {
+
+   const float RADIANS_TO_DEGREES = 57.2958; //180/3.14159
+
+   if( OpMode ==1 || OpMode==2 ) mpu_sensitivity();
+
+   // if programming failed, don't try to do anything
+   if (!dmpReady) return;
+
+   // wait for MPU interrupt or extra packet(s) available
+   while (!mpuInterrupt && fifoCount < packetSize) {
+      // other program behavior stuff here
+      // .
+      // .
+      // .
+      // if you are really paranoid you can frequently test in between other
+      // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+      // while() loop to immediately process the MPU data
+      // .
+      // .
+      // .
+   }
+
+   // reset interrupt flag and get INT_STATUS byte
+   mpuInterrupt = false;
+   mpuIntStatus = mpu.getIntStatus();
+
+   // get current FIFO count
+   fifoCount = mpu.getFIFOCount();
+
+   // check for overflow (this should never happen unless our code is too inefficient)
+   if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+      // reset so we can continue cleanly
+      mpu.resetFIFO();
+
+      // ylh
+      //Serial.println(F("FIFO overflow!"));
+      // otherwise, check for DMP data ready interrupt (this should happen frequently)
+   } 
+   else if (mpuIntStatus & 0x02) {
+      // wait for correct available data length, should be a VERY short wait
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      // read a packet from FIFO
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_QUATERNION
+      // display quaternion values in easy matrix form: w x y z
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      Serial.print("quat\t");
+      Serial.print(q.w);
+      Serial.print("\t");
+      Serial.print(q.x);
+      Serial.print("\t");
+      Serial.print(q.y);
+      Serial.print("\t");
+      Serial.println(q.z);
+#endif
+
+#ifdef OUTPUT_READABLE_EULER
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetEuler(euler, &q);
+      Serial.print("euler\t");
+      Serial.print(euler[0] * 180/M_PI);
+      Serial.print("\t");
+      Serial.print(euler[1] * 180/M_PI);
+      Serial.print("\t");
+      Serial.println(euler[2] * 180/M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      Serial.print("ypr\t");
+      Serial.print(ypr[0] * 180/M_PI);
+      Serial.print("\t");
+      Serial.print(ypr[1] * 180/M_PI);
+      Serial.print("\t");
+      Serial.println(ypr[2] * 180/M_PI);
+#endif
+
+#ifdef OUTPUT_READABLE_REALACCEL
+      // display real acceleration, adjusted to remove gravity
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      Serial.print("areal\t");
+      Serial.print(aaReal.x);
+      Serial.print("\t");
+      Serial.print(aaReal.y);
+      Serial.print("\t");
+      Serial.println(aaReal.z);
+#endif
+
+#ifdef OUTPUT_READABLE_WORLDACCEL
+      // display initial world-frame acceleration, adjusted to remove gravity
+      // and rotated based on known orientation from quaternion
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+      Serial.print("aworld\t");
+      Serial.print(aaWorld.x);
+      Serial.print("\t");
+      Serial.print(aaWorld.y);
+      Serial.print("\t");
+      Serial.println(aaWorld.z);
+#endif
+
+#ifdef OUTPUT_TEAPOT
+      // display quaternion values in InvenSense Teapot demo format:
+      if( OpMode == 0 ){
+         teapotPacket[2] = fifoBuffer[0];
+         teapotPacket[3] = fifoBuffer[1];
+         teapotPacket[4] = fifoBuffer[4];
+         teapotPacket[5] = fifoBuffer[5];
+         teapotPacket[6] = fifoBuffer[8];
+         teapotPacket[7] = fifoBuffer[9];
+         teapotPacket[8] = fifoBuffer[12];
+         teapotPacket[9] = fifoBuffer[13];
+
+         Serial.write(teapotPacket, 14);
+      } 
+      else if( OpMode == 1 ) {
+         mpu.dmpGetQuaternion(&q, fifoBuffer);
+         gClick.loop(q);
+      } 
+      else if( OpMode == 2 ){
+         // display real acceleration, adjusted to remove gravity
+         /*
+         mpu.dmpGetQuaternion(&q, fifoBuffer);
+          mpu.dmpGetAccel(&aa, fifoBuffer);
+          mpu.dmpGetGravity(&gravity, &q);
+          mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+          mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+          mpu.dmpGetEuler(euler, &q);
+          */
+
+         mpu.dmpGetGyro((int16_t *) &gyro, (uint8_t *) fifoBuffer);
+
+         gMouse.loop( gyro );
+      }      
+      else if( OpMode == 3 ){
+         mpu.dmpGetQuaternion(&q, fifoBuffer);
+         mpu.dmpGetGravity(&gravity, &q);
+         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+         Serial.print("DMP:");
+         Serial.print(ypr[0]*RADIANS_TO_DEGREES, 2);
+         Serial.print(":");
+         Serial.print(ypr[2]*RADIANS_TO_DEGREES, 2);
+         Serial.print(":");
+         Serial.println(-ypr[1]*RADIANS_TO_DEGREES, 2);
+      }
+
+      teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+#endif
+
+   }
+
+}
+
+
+void beep(int j) {
+   if (j<1) return;
+   for( int i=0; i<j; i++ ) {
+      tone(SENSACT_BUZZER, NOTE_C7, 250);
+      delay(50);
+      noTone(SENSACT_BUZZER);      
+      delay(150);
+
+      /*
+      digitalWrite( SENSACT_BUZZER, HIGH );
+       delay(10);
+       digitalWrite( SENSACT_BUZZER, LOW );
+       delay(150);
+       */
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Sensact/sensact.h
+++ Sensact/sensact.h
@@ -1,59 +1,95 @@
 
 #include "Arduino.h"
+#define SENSACT_V2
 
 const int bluetoothTx = 0;  // RX 0 of Arduino
 const int bluetoothRx = 1;  // TX 1 of Arduino
+
+#ifdef SENSACT_V1
 
 const int SENSACT_RED = 21;
 const int SENSACT_GREEN = 14;
 const int SENSACT_BLUE = 15;
 const int SENSACT_BUZZER = 10;
 
-const int SENSACT_IN1 = 0;//2;
+const int SENSACT_IN1 = 2;
 const int SENSACT_SDA = 2;
 const int SENSACT_SLK = 3;
-const int SENSACT_IN2 = 20; //A2;
-const int SENSACT_IN3 = 19; //A1;
-const int SENSACT_IN4 = 18; //A0;
+const int SENSACT_IN2 = A2; //20; //A2;
+const int SENSACT_IN3 = A1; //19; //A1;
+const int SENSACT_IN4 = A0; //8; //A0;
 
 const int SENSACT_OUT1 = 9;
 const int SENSACT_OUT2 = 6;
 const int SENSACT_OUT3 = 5;
 
+const int nInputs = 4;
+// config output indices and then the 3 values, threshold, bt char, hid char
+
+const int map_in[] = { 
+   SENSACT_IN2, SENSACT_IN3, SENSACT_IN4 };
+#endif
+
+#ifdef SENSACT_V2
+const int SENSACT_RED = A3;
+const int SENSACT_GREEN = 5;
+const int SENSACT_BLUE = 4;
+const int SENSACT_BUZZER = 10;
+
+const int SENSACT_SDA = 2;
+const int SENSACT_SLK = 3;
+const int SENSACT_IN1A = 2;
+const int SENSACT_IN1B = 3;
+
+const int SENSACT_IN2 = A2;
+const int SENSACT_IN3 = A1;  
+//const int SENSACT_IN4 = A0; //8; //A0;
+
+const int SENSACT_IR_IN = 12;
+const int SENSACT_IR_OUT = 13;
+
+const int SENSACT_OUT1 = 9;
+const int SENSACT_OUT2 = 11;
+const int nInputs = 4;
+
+const int map_in[] = { 
+   SENSACT_IN1A, SENSACT_IN1B, SENSACT_IN2, SENSACT_IN3 };
+#endif
+
 const int SENSACT_RUN = 9;
 const int SENSACT_CONFIG = 8;
 const int SENSACT_UNKNOWN = 99;
 
+
 // config output indices and then the 3 values, threshold, bt char, hid char
 
-const int map_in[] = { SENSACT_IN2, SENSACT_IN3, SENSACT_IN4 };
-
 const int 
-  I2C = 0,
-  nInputs = 4,
-  INVERT = 0,
-  RELAY_A = 1,
-  RELAY_B = 2,
-  BLUETOOTH = 3,
-  USB_HID = 4,
-  LED = 5,
-  BUZZER = 6,
-  nOutputs = 7,
-  THRESHOLD_VAL = 0,
-  BT_HID_VAL = 1,
-  USB_HID_VAL = 2,
-  nValues = 3
-  ;
-  
- /* EEPROM data structure - also stored in config[] - in bytes
-the first bytes are output controls corresponding to each input
-[nInputs][nOutputs]
+I2C = 0,
+//nInputs = 4;
+INVERT = 0,
+RELAY_A = 1,
+RELAY_B = 2,
+BLUETOOTH = 3,
+USB_HID = 4,
+LED = 5,
+BUZZER = 6,
+nOutputs = 7,
+THRESHOLD_VAL = 0,
+BT_HID_VAL = 1,
+USB_HID_VAL = 2,
+nValues = 3
+;
 
-this is followed by the values for [nInputs][nValues] - in bytes
-THRESHOLD - input trigger threshold
-BT_HID_VAL - char to output to BT
-HUSB_HID_VAL - char to output to USB HID
-*/
+/* EEPROM data structure - also stored in config[] - in bytes
+ the first bytes are output controls corresponding to each input
+ [nInputs][nOutputs]
  
-  
-  
+ this is followed by the values for [nInputs][nValues] - in bytes
+ THRESHOLD - input trigger threshold
+ BT_HID_VAL - char to output to BT
+ USB_HID_VAL - char to output to USB HID
+ */
+
+
+
+
