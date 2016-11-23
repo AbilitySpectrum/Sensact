a38f8203a3c2b5cc0d4fb12ac5bd511b5caa4e5d
adhodgson <andrew@musicvista.ca>
2016-11-23 10:37:59 -0500

    Version 3 Arduino Code
    Complete working code for Arduino side of version 3.

---
+++ Arduino/v3/SensactArduinoCode/IO.h
@@ -0,0 +1,109 @@
+// -------------------------------------
+// IO.h
+// -------------------------------------
+
+#ifndef IO_H
+#define IO_H
+
+#include <EEPROM.h>
+#include "Sensact.h"
+
+/*
+ * Code to get and put various data types from/to a stream.
+ * The types are:
+ *  char  - a char
+ *  num   - a 2 byte (4 nibble) number
+ *  long  - a 4 byte (8 nibble) number
+ *  ID    - a 1 byte (2 nibble) value
+ *  State - a 1 byte (1 nibble) value
+ *  Condition - a '1', '2' or '3'
+ *  Boolean - 'p' for true, 'q' for false
+ *
+ *  Two stream types are supported: Serial and EEPROM
+ */
+
+#define IO_ERROR -1
+class InputStream {
+  public:
+    virtual void init() = 0;
+    virtual int _getChar() = 0;
+    
+    int getChar();    // Filters out end-of-line characters.
+    int  getNum() { return _getNumber(4); }
+    long getLong() { return _getNumber(8); }
+    char getID() { return _getChar(2); }
+    char getState() { return _getChar(1); }
+    char getCondition();
+    char getBool(); 
+    long _getNumber(int nbytes);
+    char _getChar(int nBytes);
+};
+
+class OutputStream {
+  public:
+    virtual void init() = 0;
+    virtual void putChar(int c) = 0;
+    
+    void putLong(long val) { _putNumber(val, 8); }
+    void putNum(int val) { _putNumber(val, 4); }   
+    void putID(char ID);
+    void putState(char state);
+    void putCondition(char condition);
+    void putBool(char b);
+
+    void _putNumber(long val, int nbytes);
+};
+
+// --- Serial Port I/O --- //
+class SerialInputStream : public InputStream {
+  public:
+    void init() {}
+    
+    int _getChar() {
+      int count = 0;
+      while (!Serial.available()) {
+        delay(10);
+        count++;
+        if (count > 50) { // Waiting for over 1/2 second
+          return IO_ERROR;
+        }
+      }
+      return Serial.read();
+    }
+};
+
+class SerialOutputStream : public OutputStream {
+  public:
+    void init() {}
+    void putChar(int c) {
+      Serial.write(c);
+    }
+};
+
+// --- EEPROM I/O --- //
+
+class EEPROMInputStream : public InputStream {
+  private:
+    int index;
+  
+  public:
+    EEPROMInputStream() { index = 0; }
+    void init() { index = 0; }
+    int _getChar() {
+      return EEPROM.read(index++);
+    }
+};
+
+class EEPROMOutputStream : public OutputStream {
+  private:
+    int index;
+    
+  public:
+    EEPROMOutputStream() { index = 0; }
+    void init() { index = 0; }
+    void putChar(int c) {
+      EEPROM.write(index++, c);
+    }
+};
+    
+#endif
---
+++ Arduino/v3/SensactArduinoCode/Sensact.h
@@ -0,0 +1,61 @@
+// --------------------------------------
+// Sensors.h
+// --------------------------------------
+#ifndef SENSACT_H
+#define SENSACT_H
+
+#include <Arduino.h>
+// Various Constants used throughout the code //
+
+// === Protocol Values === //
+// -- Commands and Block Headers -- //
+#define REPORT_MODE       'Q'
+#define RUN_SENSACT       'R'
+#define START_OF_SENSOR_DATA 'S'
+#define START_OF_TRIGGER_BLOCK 'T'
+#define REQUEST_TRIGGERS  'U'
+#define GET_VERSION       'V'
+#define MIN_COMMAND  'Q'
+#define MAX_COMMAND  'V'
+
+// -- Data block separators -- //
+#define TRIGGER_START  't'
+#define TRIGGER_END    'z'
+#define END_OF_BLOCK   'Z'
+
+// -- Value encoding -- //
+#define NUMBER_MASK 0x60
+#define ID_MASK     0x40
+#define CONDITION_MASK '0'
+#define BOOL_TRUE   'p'
+#define BOOL_FALSE  'q'
+
+// === LED Pins === //
+#define LED_RED    A3
+#define LED_GREEN  5
+#define LED_BLUE   4
+
+// === Sensor Pins === //
+#define SENSACT_IN2   A2
+#define SENSACT_IN3   A1
+
+// === Action Pins === //
+#define SENSACT_IR_OUT  13
+#define SENSACT_BUZZER  10
+#define SENSACT_RELAY_1  9
+#define SENSACT_RELAY_2 11
+
+// === Timing Constants - all in ms === //
+#define REPORTING_INTERVAL  200  // Interval between reports of signal levels
+#define READING_INTERVAL     50  // Interval between reading signals in run mode
+#define REFRACTORY          800  // Interval within which the output will not re-trigger.
+#define PULSE_WIDTH          50  // Output pulse width
+#define REPEAT_INTERVAL     250  // Time between repeats of repeated actions.
+
+// ==== Some Limits === //
+#define MAX_TRIGGERS 20           // Maximum number of triggers allowed.
+#define MAX_ACTIONS 15            // Maximum number of actions allowed per trigger check.
+#define MAX_SENSORS 10
+#define MAX_ACTORS  10
+
+#endif
