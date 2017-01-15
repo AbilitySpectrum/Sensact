91f3f419666a7bdeefade4b916d5de922fef045c
adhodgson <andrew@musicvista.ca>
2017-01-14 21:44:31 -0500

    Make mouse-button typing faster.
    Make mouse move faster as repeat action is continued.

--- Arduino/v3/SensactArduinoCode/Sensact.h
+++ Arduino/v3/SensactArduinoCode/Sensact.h
@@ -48,10 +48,10 @@
 
 // === Timing Constants - all in ms === //
 #define REPORTING_INTERVAL  200  // Interval between reports of signal levels
-#define READING_INTERVAL     50  // Interval between reading signals in run mode
+#define READING_INTERVAL     10  // Interval between reading signals in run mode
 #define REFRACTORY          800  // Interval within which the output will not re-trigger.
 #define PULSE_WIDTH          50  // Output pulse width
-#define REPEAT_INTERVAL     250  // Time between repeats of repeated actions.
+#define REPEAT_INTERVAL     100  // Time between repeats of repeated actions.
 
 // ==== Some Limits === //
 #define MAX_TRIGGERS 15           // Maximum number of triggers allowed.
