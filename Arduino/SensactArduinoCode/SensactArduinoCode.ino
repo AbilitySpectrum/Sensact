45ccc511941c3374de19ce607372dab801668102
adhodgson <andrew@musicvista.ca>
2018-10-28 19:33:55 -0400

    License header added to Arduino code

--- Arduino/SensactArduinoCode/SensactArduinoCode.ino
+++ Arduino/SensactArduinoCode/SensactArduinoCode.ino
@@ -1,14 +1,20 @@
-// ---------------------------------------
-// Sensact code - Version 3.
-// This is a complete re-think of the project.
-// The class structure has been redefined.  Sensors, triggers and actions
-// are in isolated blocks of code which share only the information they have to share.
-//
-// Previously the system was centered on sensors which could have associated actions.
-// This version is centered on triggers, which are associated with a sensor and an action.
-// The addition of triggering states makes it possible to assemble the triggers into state
-// machines which can be the basis for getting multiple functions from a single sensor.
-// ----------------------------------------
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+    This file is part of the Sensact Arduino software.
+
+    Sensact Arduino software is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Sensact Arduino software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this Sensact Arduino software.  
+    If not, see <https://www.gnu.org/licenses/>.   
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * */ 
 
 #include "Sensact.h"
 #include "Sensors.h"
